d82771c1701cdfab3e81972ec7321af683054ca8
Yancel Moises <sagoyanfisic1@gmail.com>
2015-12-16 19:44:11 -0500

    actualizando la carpeta web

--- taskman/personal.php
+++ taskman/personal.php
@@ -11,15 +11,14 @@
 		<p id="bienvenidot"><a href="" id="end">Salir</a> Bienvenido, <b></b></p>
 	</div>
 	<div id="botones">
-		<a href="calendar/index.php"><p>Calendario</p></a>
-		<a href="index.php"><p>Historial de Procesos Judiciales</p></a>
-		<a href="correo.php"><p>Correo</p></a>
-		<a href="archivos.php"><p>Guardar Archivos</p></a>
+		<a href="calendar/index.php"><p>CURSO</p></a>
+		<a href="index.php"><p>USUARIO</p></a>
+		<a href="correo.php"><p>FORO DE PROFESORES</p></a>
 	</div>
 	<div id="tarjetas">
 		<br>
 		<div id="capsulas" style=" background:rgb(197, 182, 228);">
-			<h2>Eventos</h2>
+			<h2>Mensaje de Bienvenida</h2>
 			<hr>
 			
 
